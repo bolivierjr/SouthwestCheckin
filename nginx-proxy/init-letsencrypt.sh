a352aec4b46bda2b83cccc6427c66f47086bb38e
Bruce Olivier <bolivierjr@gmail.com>
2019-10-12 09:59:49 -0500

    Fix: Had to update the url in the certbot script and change the data path to the correct directory.

--- nginx-proxy/init-letsencrypt.sh
+++ nginx-proxy/init-letsencrypt.sh
@@ -7,8 +7,8 @@ fi
 
 domains=(example.org www.example.org)
 rsa_key_size=4096
-data_path="./nginx-proxy/certbot"
-email="bolivierjr@gmail.com" # Adding a valid address is strongly recommended
+data_path="./certbot"
+email="example@example.org" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
@@ -22,7 +22,7 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
