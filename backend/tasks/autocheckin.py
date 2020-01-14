709478d6f2da2e45b9939e77dc16ccfa3a2fe68e
Bruce Olivier <bolivierjr@gmail.com>
2020-01-14 00:17:19 -0600

    Fix: removed gevent from gunicorn and just made 3 workers for the uwgsi server. Fix: changed the stdout redirect back to sys.stdout at the end of the autocheckin task. Removed os.waitpid() since there are no more child processes to wait for.

--- backend/tasks/autocheckin.py
+++ backend/tasks/autocheckin.py
@@ -13,13 +13,18 @@ def autocheckin(confirmation, firstname, lastname, notifications):
     """
     Worker for multiprocessing Process.
     """
+    _stdout = sys.stdout
+
     try:
         redis_client.hmset(confirmation, {"running": "True", "PID": os.getpid()})
         redis_client.expire(confirmation, timedelta(days=60))
 
         sys.stdout = RedisStream(confirmation, redis_client)
         auto_checkin(confirmation, firstname, lastname, notifications)
+
     except (SystemExit, Exception) as exc:
         print(exc)
 
-    redis_client.hset(confirmation, "running", "False")
+    finally:
+        sys.stdout = _stdout
+        redis_client.hset(confirmation, "running", "False")
